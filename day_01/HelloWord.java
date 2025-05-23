0a5aebbe4cc27a38a34298b35151adf73ce6343a
PcarloP <carlopapa111@gmail.com>
2025-05-23 15:39:45 +0200

    first commit

---
+++ day_01/HelloWord.java
@@ -0,0 +1,11 @@
+package day_01;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" hello word ");
+	}
+
+}
---
+++ day_02/HelloWord.java
@@ -0,0 +1,11 @@
+package day_02;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println( "hello word ! day 2") ;
+	
+	}
+
+}
